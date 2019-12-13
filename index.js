b6e2f7d147ca4f032aec0d54a48c65cb5a909098
ThomLlobEce <36538693+ThomLlobEce@users.noreply.github.com>
2019-12-13 17:43:35 +0100

    /api/isAuth added

--- index.js
+++ index.js
@@ -3,44 +3,59 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser')
 
-const users = []
+const users = [{name: "Llobregat", firstName: "Thomas", email: "w@gmail.com", password: "azer"}]
+const auths = []
 
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+.use(express.static(path.join(__dirname, 'client/build')))
 
 // An api endpoint that returns a short list of items
-app.post('/api/createUser', (req,res) => {
+.post('/api/createUser', (req,res) => {
 
     users.push({name: req.body.last_name, firstName: req.body.first_name, email: req.body.email, password: req.body.password})
 
     console.log(users)
 
     res.send("User correctly added")
-});
+})
 
 // An api endpoint that returns a short list of items
-app.get('/api/logIn', (req,res) => {
+.get('/api/signIn', (req,res) => {
     let user = false
 
-    users.forEach( u => {
-        if(u.email === req.query.email && u.password === req.query.password){
-            user = u
+    for(let i = 0; i < users.length; i++){
+        if(users[i].email === req.query.email && users[i].password === req.query.password){
+            user = users[i]
+            auths.push(users[i].email)
             break
         }
-    })
+    }
 
-    res.send(u)
-});
+    res.send(user)
+})
+
+.get('/api/isAuth', (req, res) => {
+    let auth = false
+
+    for(let i = 0; i < auths.length ; i++){
+        if(auths[i] === req.query.email){
+            auth = true
+        }
+    }
+
+    res.send(auth)
+
+})
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
+.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+})
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+.listen(port);
 
 console.log('App is listening on port ' + port);
