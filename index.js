8a1927f1d925c6600613a6e4f2932fd2491cfee1
ThomLlobEce <36538693+ThomLlobEce@users.noreply.github.com>
2019-12-13 16:57:39 +0100

    API signUp & logIn created

--- index.js
+++ index.js
@@ -3,18 +3,44 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser')
 
-const PORT = 8800;
+const users = []
+
 
 app.use(bodyParser.json())
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+// An api endpoint that returns a short list of items
+app.post('/api/createUser', (req,res) => {
+
+    users.push({name: req.body.last_name, firstName: req.body.first_name, email: req.body.email, password: req.body.password})
+
+    console.log(users)
+
+    res.send("User correctly added")
+});
+
+// An api endpoint that returns a short list of items
+app.get('/api/logIn', (req,res) => {
+    let user = false
+
+    users.forEach( u => {
+        if(u.email === req.query.email && u.password === req.query.password){
+            user = u
+            break
+        }
+    })
+
+    res.send(u)
+});
+
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.listen(PORT);
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-console.log('App is listening on port ' + PORT);
+console.log('App is listening on port ' + port);
