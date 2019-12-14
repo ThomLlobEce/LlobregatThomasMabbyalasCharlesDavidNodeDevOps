5be4cea13cb38c5c992681f79be73cebccd33d9e
charles <charles-david.mabbyalas@edu.ece.fr>
2019-12-14 18:45:41 +0100

    MaJ de la branche charles

---
+++ client/build/precache-manifest.673fe5bc9ca7d0d6ed4cd52636ab6b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0decc41dca192f88c1dd3045c7485c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3899f1f89395d44096eb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7071b942095b13bc0373",
+    "url": "/static/js/2.a4007c42.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.a4007c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3899f1f89395d44096eb",
+    "url": "/static/js/main.7b8e35ed.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
