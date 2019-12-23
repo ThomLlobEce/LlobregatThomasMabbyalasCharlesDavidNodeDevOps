dd3891b0fdfa2cb34c302a7afa48c70df9e9e650
ThomLlobEce <36538693+ThomLlobEce@users.noreply.github.com>
2019-12-23 17:56:48 +0100

    Use of typescript

--- app.js
+++ app.js
@@ -1,130 +1,118 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const bodyParser = require('body-parser')
-
-const users = []
-const auths = []
-
-const port = process.env.PORT || 5000;
-
+"use strict";
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var User = /** @class */ (function () {
+    function User(name, firstName, email, password) {
+        this.name = name;
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+    }
+    return User;
+}());
+var users = [];
+var auths = [];
+var port = process.env.PORT || 5000;
 app.use(bodyParser.json());
-
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.post('/api/createUser', (req,res) => {
-    let exist = false
-    let missingParams = false;
-
-    if(!req.body.email || !req.body.first_name || !req.body.last_name || !req.body.password){
-        missingParams = true
+app.post('/api/createUser', function (req, res) {
+    var exist = false;
+    var missingParams = false;
+    if (!req.body.email || !req.body.first_name || !req.body.last_name || !req.body.password) {
+        missingParams = true;
     }
-    else{
-        for(let i = 0; i < users.length; i++){
-            if(users[i].email === req.body.email){
-                exist = true
-                break
+    else {
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].email === req.body.email) {
+                exist = true;
+                break;
             }
         }
     }
-    
-    if(exist){
+    if (exist) {
         res.json({
             status: "failed",
             message: "Email already exists"
-        })
+        });
     }
-    else if(missingParams){
+    else if (missingParams) {
         res.json({
             status: "failed",
             message: "Missing parameters"
-        })
+        });
     }
-    else{
-        users.push({name: req.body.last_name, firstName: req.body.first_name, email: req.body.email, password: req.body.password})
+    else {
+        users.push({ name: req.body.last_name, firstName: req.body.first_name, email: req.body.email, password: req.body.password });
         res.json({
             status: "success",
             message: "User added"
-          });
+        });
     }
 });
-
-app.get('/api/signIn', (req,res) => {
-    let user = false
-    let exist = false
-
-    for(let i = 0; i < users.length; i++){
-        if(users[i].email === req.query.email && users[i].password === req.query.password){
-            for(let j = 0; j < auths.length; j++){
-                if(auths[j] = users[i].email){
-                    exist = true
-                    break
+app.get('/api/signIn', function (req, res) {
+    var user = new User("", "", "", "");
+    var exist = false;
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].email === req.query.email && users[i].password === req.query.password) {
+            for (var j = 0; j < auths.length; j++) {
+                if (auths[j] = users[i].email) {
+                    exist = true;
+                    break;
                 }
             }
-            if(!exist){
-                user = users[i]
-                auths.push(users[i].email)
+            if (!exist) {
+                user = users[i];
+                auths.push(users[i].email);
             }
-            break
+            break;
         }
     }
-
-    if(user){
+    if (user) {
         res.json({ status: "success", message: user });
     }
-    else if(exist){
+    else if (exist) {
         res.json({ status: "failed", message: "Already signed in" });
     }
-    else{
+    else {
         res.json({ status: "failed", message: "error" });
     }
 });
-
-app.get('/api/isAuth', (req, res) => {
-    let auth = false
-
-    for(let i = 0; i < auths.length ; i++){
-        if(auths[i] === req.query.email){
-            auth = true
+app.get('/api/isAuth', function (req, res) {
+    var auth = false;
+    for (var i = 0; i < auths.length; i++) {
+        if (auths[i] === req.query.email) {
+            auth = true;
         }
     }
-
-    if(auth){
+    if (auth) {
         res.json({ status: "success", message: auth });
     }
-    else{
+    else {
         res.json({ status: "failed", message: auth });
     }
-
 });
-
-app.get('/api/disconnect', (req, res) => {
-    let disconnect = false
-
-    for(let i = 0; i < auths.length ; i++){
-        if(auths[i] === req.query.email){
-            auths.splice(i, 1)
-            disconnect = true
+app.get('/api/disconnect', function (req, res) {
+    var disconnect = false;
+    for (var i = 0; i < auths.length; i++) {
+        if (auths[i] === req.query.email) {
+            auths.splice(i, 1);
+            disconnect = true;
         }
     }
-
-    if(disconnect){
+    if (disconnect) {
         res.json({ status: "success", message: disconnect });
     }
-    else{
+    else {
         res.json({ status: "failed", message: disconnect });
     }
-
 });
-
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
-
 app.listen(port);
-
 console.log('App is listening on port ' + port);
-
-module.exports = app
+module.exports = app;
---
+++ app.ts
@@ -0,0 +1,144 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser')
+
+class User {
+    name: string
+    firstName: string
+    email: string
+    password: string
+  
+    constructor(name: string, firstName: string, email: string, password: string) {
+      this.name = name
+      this.firstName = firstName
+      this.email = email
+      this.password = password
+    }
+  }
+
+const users: User[] = []
+const auths: string[] = []
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.post('/api/createUser', (req: { body: { email: string; first_name: string; last_name: string; password: string; }; },res: { json: (arg0: { status: string; message: string; }) => void; }) => {
+    let exist = false
+    let missingParams = false;
+
+    if(!req.body.email || !req.body.first_name || !req.body.last_name || !req.body.password){
+        missingParams = true
+    }
+    else{
+        for(let i = 0; i < users.length; i++){
+            if(users[i].email === req.body.email){
+                exist = true
+                break
+            }
+        }
+    }
+    
+    if(exist){
+        res.json({
+            status: "failed",
+            message: "Email already exists"
+        })
+    }
+    else if(missingParams){
+        res.json({
+            status: "failed",
+            message: "Missing parameters"
+        })
+    }
+    else{
+        users.push({name: req.body.last_name, firstName: req.body.first_name, email: req.body.email, password: req.body.password})
+        res.json({
+            status: "success",
+            message: "User added"
+          });
+    }
+});
+
+app.get('/api/signIn', (req: { query: { email: string; password: string; }; },res: { json: (arg0: { status: string; message: string | User ; }) => void; }) => {
+    let user =  new User("", "", "", "")
+    let exist = false
+
+    for(let i = 0; i < users.length; i++){
+        if(users[i].email === req.query.email && users[i].password === req.query.password){
+            for(let j = 0; j < auths.length; j++){
+                if(auths[j] = users[i].email){
+                    exist = true
+                    break
+                }
+            }
+            if(!exist){
+                user = users[i]
+                auths.push(users[i].email)
+            }
+            break
+        }
+    }
+
+    if(user){
+        res.json({ status: "success", message: user });
+    }
+    else if(exist){
+        res.json({ status: "failed", message: "Already signed in" });
+    }
+    else{
+        res.json({ status: "failed", message: "error" });
+    }
+});
+
+app.get('/api/isAuth', (req: { query: { email: string; }; }, res: { json: (arg0: { status: string; message: boolean; }) => void; }) => {
+    let auth = false
+
+    for(let i = 0; i < auths.length ; i++){
+        if(auths[i] === req.query.email){
+            auth = true
+        }
+    }
+
+    if(auth){
+        res.json({ status: "success", message: auth });
+    }
+    else{
+        res.json({ status: "failed", message: auth });
+    }
+
+});
+
+app.get('/api/disconnect', (req: { query: { email: string; }; }, res: { json: (arg0: { status: string; message: boolean; }) => void; }) => {
+    let disconnect = false
+
+    for(let i = 0; i < auths.length ; i++){
+        if(auths[i] === req.query.email){
+            auths.splice(i, 1)
+            disconnect = true
+        }
+    }
+
+    if(disconnect){
+        res.json({ status: "success", message: disconnect });
+    }
+    else{
+        res.json({ status: "failed", message: disconnect });
+    }
+
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req: any,res: { sendFile: (arg0: any) => void; }) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port);
+
+console.log('App is listening on port ' + port);
+
+module.exports = app
