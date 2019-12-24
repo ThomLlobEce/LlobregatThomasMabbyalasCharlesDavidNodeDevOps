b07a642015470a4dffb2aa578b604128d3302b38
Th0rr <vicgregoire@hotmail.fr>
2019-12-24 17:21:26 +0100

    a

--- client/src/Components/Metrics.js
+++ client/src/Components/Metrics.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 import NavBar from './NavBar';
 const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 class Metrics extends Component {
 
